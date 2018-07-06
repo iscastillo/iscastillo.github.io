bcc21d5a8e8ca43f8832ec668b2829fb15e40a3c
Kevin Pham <kevinpham565@gmail.com>
2018-07-06 11:06:28 -0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,12 +41,4 @@
 				resetForms: true,
 				side: 'right'
 			});
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
 })(jQuery);
